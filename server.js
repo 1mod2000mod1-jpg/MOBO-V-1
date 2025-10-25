4cb6b8672ffa16ae13b045ed1e365bff5ea2f897
1mod2000mod1-jpg <1mod2000mod1@gmail.com>
2025-10-25 15:51:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1093,7 +1093,7 @@ io.on('connection', (socket) => {
       };
 
       io.emit('social-links-updated', systemSettings.socialLinks);
-      socket.emit('action-success', '✅ تم تحديث روابط التواصل');
+      socket.emit('action-success', '');
       
     } catch (error) {
       console.error('خطأ في تحديث الروابط:', error);
